4cc76e7e378b70432803b7ee7311b043c3ffbb51
thesmuniz <steven.muniz@gmail.com>
2019-10-21 16:21:01 -0500

    Creating Items with Mutations to pass the data to our Prisma layer

--- sick-fits/frontend/.next/static/runtime/webpack.js
+++ sick-fits/frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "8498961a8be0f853d0ad";
+/******/ 	var hotCurrentHash = "84075777dd91827cdd14";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
