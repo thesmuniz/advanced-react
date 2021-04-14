abe41a4dbf4594bee652e131fe90368469ce5cd6
Steven Muniz <steven.muniz@gmail.com>
2021-04-13 22:39:32 -0500

    Completion of Module 3 - Css & Styled Components

---
+++ sick-fits/frontend/pages/_app.js
@@ -0,0 +1,16 @@
+import Nprogress from 'nprogress';
+import Router from 'next/router';
+import Page from '../components/Page';
+import '../components/styles/nprogress.css'
+
+Router.events.on('routeChangeStart', () => Nprogress.start());
+Router.events.on('routeChangeComplete', () => Nprogress.done());
+Router.events.on('routeChangeError', () => Nprogress.done());
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <Page>
+      <Component {...pageProps} />
+    </Page>
+  );
+}
